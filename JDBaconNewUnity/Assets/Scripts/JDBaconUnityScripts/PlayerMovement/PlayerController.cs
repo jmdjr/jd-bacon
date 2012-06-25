1c862e2fa8826f26586a01c4213388e4f8b74e8e
jmdcodejunkie@gmail.com <jmdcodejunkie@gmail.com@f267d39f-6551-975b-9700-a0f0e956190a>
2012-06-25 02:37:22 +0000

    Migrated Scripts to their own directory. Added new StateManagement system into library. Added Orthello scripts to the library for spritemap control.

---
+++ JDBaconNewUnity/Assets/Scripts/JDBaconUnityScripts/PlayerMovement/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : GameObjectStateMachine
+{
+    private GameObjectState Idle = new GameObjectState("Idle");
+    private GameObjectState WalkingRight = new GameObjectState("Walking to the Right");
+    private GameObjectState WalkingLeft = new GameObjectState("Walking to the Left");
+    private GameObjectState JumpingUp = new GameObjectState("Jumping into the Air");
+    private GameObjectState DoubleJumpingUp = new GameObjectState("2nd Jumping into the Air");
+
+    public float WalkingSpeed = 1f;
+    public float JumpingSpeed = 2.0f;
+    public bool AllowDoubleJump = true;
+
+    private bool airborne = false;
+    private bool airjumped = false;
+
+    protected override void InitializeStateManager()
+    {
+        SetInitialState(Idle);
+        
+        ExitStateCondition ToWalkingLeft = new ExitStateCondition(ToWalkingLeftCondition, WalkingLeft);
+        ExitStateCondition ToWalkingRight = new ExitStateCondition(ToWalkingRightCondition, WalkingRight);
+        ExitStateCondition ToIdle = new ExitStateCondition(ToIdleCondition, Idle);
+        ExitStateCondition ToJumpingUp = new ExitStateCondition(ToJumpCondition, JumpingUp);
+        ExitStateCondition ToDoubleJumpingUp = new ExitStateCondition(ToDoubleJumpCondition, DoubleJumpingUp);
+
+        Idle.Action = IdleAction;
+        Idle.AddExitCondition(ToWalkingLeft);
+        Idle.AddExitCondition(ToWalkingRight);
+        Idle.AddExitCondition(ToJumpingUp);
+
+        WalkingRight.Action = WalkingRightAction;
+        WalkingRight.RepeatActionCount = 0;
+        WalkingRight.AddExitCondition(ToWalkingLeft);
+        WalkingRight.AddExitCondition(ToIdle);
+        WalkingLeft.AddExitCondition(ToJumpingUp);
+
+        WalkingLeft.Action = WalkingLeftAction;
+        WalkingLeft.RepeatActionCount = 0;
+        WalkingLeft.AddExitCondition(ToWalkingRight);
+        WalkingLeft.AddExitCondition(ToIdle);
+        WalkingLeft.AddExitCondition(ToJumpingUp);
+    }
+
+    #region Actions
+    private IEnumerator IdleAction()
+    {
+        this.airjumped = false;
+        this.airborne = false;
+
+        yield return 0;
+    }
+    private IEnumerator WalkingRightAction()
+    {
+        this.rigidbody.velocity = new Vector3(WalkingSpeed * -1, this.rigidbody.velocity.y);
+        yield return 0;
+    }
+    private IEnumerator WalkingLeftAction()
+    {
+        this.rigidbody.velocity = new Vector3(WalkingSpeed * 1, this.rigidbody.velocity.y);
+        yield return 0;
+    }
+    private IEnumerator JumpingUpEntered()
+    {
+        airborne = true;
+        yield return 0;
+    }
+    private IEnumerator DoubleJumpingUpEntered()
+    {
+        airborne = true;
+        yield return 0;
+    }
+    private IEnumerator JumpingUpAction()
+    {
+        this.rigidbody.AddRelativeForce(0.0f, 0.0f, JumpingSpeed * Input.GetAxis("Vertical"), ForceMode.Impulse); 
+        yield return 0;
+    }
+    #endregion
+
+    #region Conditions
+    private bool ToWalkingRightCondition()
+    {
+        return Input.GetAxis("Horizontal") > 0;
+    }
+    private bool ToWalkingLeftCondition()
+    {
+        return Input.GetAxis("Horizontal") < 0;
+    }
+    private bool ToIdleCondition()
+    {
+        return Input.GetAxis("Horizontal") == 0;
+    }
+    private bool ToJumpCondition()
+    {
+        return Input.GetAxis("Vertical") > 0 && !this.airborne;
+    }
+    private bool ToDoubleJumpCondition()
+    {
+        return Input.GetAxis("Vertical") > 0 && !this.airjumped;
+    }
+    #endregion
+
+
+
+    // it should still be fine for a state machine to have an update function, so long as the update function does not
+    //  attempt to alter the flow of the statemachine directly.
+    public void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0 && !this.airborne)
+        {
+            this.airborne = true;
+            this.rigidbody.AddRelativeForce(0.0f, 0.0f, JumpingSpeed * Input.GetAxis("Vertical"), ForceMode.Impulse);
+        }
+    }
+
+}
---
+++ JDBaconNewUnity/Assets/Scripts/JDBaconUnityScripts/UtilityScripts/Camera/SLerpTransitions.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+using Object = UnityEngine.Object;
+using System.Collections.Generic;
+
+public class SLerpTransitions : MonoBehaviour
+{
+    public Object CameraRig;
+    public float TransitionDuration = 2.5f;
+
+    // Amound of time between each transition.
+    public float IdleDuration = 1.0f;
+    public bool RestartWhenDone = false;
+    
+    public Array stuffs;
+    private List<Transform> positionList;
+    private Transform curTransform;
+    private int curTransformIndex;
+
+    private bool atEndOfPositionList
+    {
+        get { return curTransformIndex == positionList.Count - 1; }
+    }
+
+    IEnumerator IdleForSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+    IEnumerator FollowTransitions()
+    {
+        while (RestartWhenDone || !atEndOfPositionList)
+        {
+            yield return StartCoroutine(MoveToNextLocationList());
+            yield return StartCoroutine(TransitionTo(curTransform));
+            yield return StartCoroutine(IdleForSeconds(IdleDuration));
+        }
+    }
+    IEnumerator TransitionTo(Transform positionOrientation)
+    {
+        float t = 0.0f;
+        Vector3 startingPos = transform.position;
+        Quaternion startingOri = transform.rotation;
+
+        Vector3 position = positionOrientation.position;
+        Quaternion orientation = positionOrientation.rotation;
+
+        if (position != Vector3.zero || orientation != Quaternion.identity)
+        {
+            while (t < 1.0f)
+            {
+                t += Time.deltaTime * (Time.timeScale / TransitionDuration);
+
+                if (position != Vector3.zero)
+                {
+                    transform.position = Vector3.Lerp(startingPos, position, t);
+                }
+
+                if (orientation != Quaternion.identity)
+                {
+                    transform.rotation = Quaternion.Slerp(startingOri, orientation, t);
+                }
+
+                yield return 0;
+            }
+        }
+    }
+
+    public void Awake()
+    {
+        curTransformIndex = 0;
+        positionList = new List<Transform>();
+        GameObject obj = CameraRig as GameObject;
+
+        if(obj != null)
+        {
+            int childCount = obj.transform.GetChildCount();
+            int iterator = 0;
+
+            while (iterator < childCount)
+            {
+                positionList.Add(obj.transform.GetChild(iterator));
+                ++iterator;
+            }
+        }
+
+        StartCoroutine(FollowTransitions());
+    }
+
+    //public void Update()
+    //{
+    //}
+
+    //public void MoveToNextLocation()
+    //{
+    //    if (curTransform == pos0)
+    //    {
+    //        Debug.Log("Switched to Pos 1");
+    //        curTransform = pos1;
+    //    }
+    //    else if (curTransform == pos1)
+    //    {
+    //        Debug.Log("Switched to Pos 2");
+    //        curTransform = pos2;
+    //    }
+    //    else if (curTransform == pos2)
+    //    {
+    //        Debug.Log("Switched to Pos 0");
+    //        curTransform = pos0;
+    //    }
+    //}
+
+    IEnumerator MoveToNextLocationList()
+    {
+        if (RestartWhenDone && atEndOfPositionList)
+        {
+            curTransformIndex = 0;
+        }
+        else
+        {
+            ++curTransformIndex;
+        }
+
+        curTransform = positionList[curTransformIndex];
+        yield return 0;
+    }
+}
