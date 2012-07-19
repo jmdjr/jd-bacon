cfdf337a2f9413352bcb580382dcb06cb91df4c1
jmdcodejunkie@gmail.com <jmdcodejunkie@gmail.com@f267d39f-6551-975b-9700-a0f0e956190a>
2012-07-19 05:14:07 +0000

    John Davis Jr: - Added new Prefabs/TriggerPrefabs/_ZeroZoneBox.prefab which is used to trigger death upon things that exit too far out of the level scope. - Added ZeroZoneScript to kill things that fall too far out of level. Player reference is just stopped and made invisible. - Updated LevelOne.unity to include useage of ZeroZoneScript and _ZeroZoneBox.prefab.

---
+++ JDBaconNewUnity/Assets/Scripts/JDBaconUnityScripts/UtilityScripts/ZeroZoneScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq.Expressions;
+
+using Object = UnityEngine.Object;
+using System.Collections.Generic;
+
+public class ZeroZoneScript : MonoBehaviour 
+{
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.rigidbody.useGravity = false;
+            other.gameObject.renderer.enabled = false;
+            other.gameObject.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            Debug.Log("hit zero zone");
+        }
+        else
+        {
+            Destroy(other);
+        }
+
+    }
+}
