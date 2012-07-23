d399086eda7a1528b62fd117e01a07cddc4700f3
jmdcodejunkie@gmail.com <jmdcodejunkie@gmail.com@f267d39f-6551-975b-9700-a0f0e956190a>
2012-07-23 07:06:42 +0000

    John Davis Jr:
    - Reorganized the scripts folders so that there is some consistency in the placement of scripts.

---
+++ JDBaconNewUnity/Assets/Scripts/JDBaconUnityScripts/StatisticsManagement/JDGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+using Object = UnityEngine.Object;
+using System.Collections.Generic;
+
+public class JDGame
+{
+    private static GrimReaper reaper;
+    public static GrimReaper GrimReaper
+    {
+        get
+        {
+            if(reaper == null)
+            {
+                GameObject LevelMaster = GameObject.Find("__LevelMasterObject");
+                reaper = ((GrimReaper)LevelMaster.GetComponent<GrimReaper>());
+            }
+
+            return reaper;
+        }
+    }
+}
+
