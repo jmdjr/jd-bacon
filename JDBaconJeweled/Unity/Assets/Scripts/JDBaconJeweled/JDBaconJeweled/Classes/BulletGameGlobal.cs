5234d50ca84112af2d98cc6bfde0e2e0a9a13f27
jmdcodejunkie <jmdcodejunkie@f267d39f-6551-975b-9700-a0f0e956190a>
2012-12-28 06:43:40 +0000

    updates for base debug settings.

--- JDBaconJeweled/Unity/Assets/Scripts/JDBaconJeweled/JDBaconJeweled/Classes/BulletGameGlobal.cs
+++ JDBaconJeweled/Unity/Assets/Scripts/JDBaconJeweled/JDBaconJeweled/Classes/BulletGameGlobal.cs
@@ -32,6 +32,7 @@ public class BulletGameGlobal
 
     private void BulletGameGlobal_Debug_GlobalSet(string[] Params)
     {
+        Debug.Log(Params.Length);
         if(Params.Length == 0 || DebugCommands.IsHelpSwitch(Params[0]))
         {
             Debug.Log(
